463f734a92856476cfa35be0c9b6e4670dc9e659
Adam Sardar <adam@sardar.me.uk>
2012-02-10 17:52:23 +0000

    Complete overhaul of Hist.py. Improved command line parser and added ability to log transform data

--- Hist.py
+++ Hist.py
@@ -1,65 +1,58 @@
-#! /usr/local/bin/sage --python
-#Using the sage implementation of python for nice easy access to scilab and matlibplot etc
+#!/usr/bin/python
 
 # Hist.py -f inputdata as colon seperated file -o Outputpath(with file extension) -t "Hist title" -x "xlabel" -y "ylabel"
 
 import numpy as np
 import pylab as pl
 import sys
-import getopt
+import argparse
+import math
 
-def main(argv):
+def main():
 
-	(binsize,colour,title,x_lab,y_lab,delim,label,column) = (75,'#7D7D7D','Hisogram Of Data','Value','Frequency',':','data',1)
+	#(binsize,colour,title,x_lab,y_lab,delim,label,column) = (75,'#7D7D7D','Hisogram Of Data','Value','Frequency',':','data',1)
 
-	try:
-		opts, args = getopt.getopt(argv, "f:o:b:c:t:x:y:d:l:u", ["File=", "Output=","bins=","colour=","title=","xlab=","ylab=","delim=","label=","column=","vline="])	
-	except getopt.GetoptError:
-		print "Input and Output filenames required along with data label"
-		exit(2)
+	parser = argparse.ArgumentParser(description='Produce histogram plots from input data using Matplot lib. Install SAGE math so that you can access the numpy and pylab modules easily.')
+		
+	parser.add_argument('--file', '-f', metavar='FILE', required=True, type=argparse.FileType('r'), dest='InputFile', help='File of data that you wish to turn into a histogram')
+	parser.add_argument('--output', '-o', metavar='OUTPUT', required=True, dest='Output', type=argparse.FileType('w'), help='Output image file (with file extension) - e.g Hist.png')
+	parser.add_argument('--bins', '-b', metavar='BINS', required=False, dest='binsize', type=int, default=75, help='Number of bins in output image')
 	
-	for opt, arg in opts:
-		if opt in ('-f','--File'):
-			InputFile = arg
-		elif opt in ('-o','--Output'):
-			Output = arg
-		elif opt in ('-b','--bins'):
-			binsize = int(arg)
-		elif opt in ('-c','--colour'):
-			colour = arg
-		elif opt in ('-t','--title'):
-			title = arg
-		elif opt in ('-x','--xlab'):
-			x_lab = arg
-		elif opt in ('-y','--ylab'):
-			y_lab = arg
-		elif opt in ('-d','--delim'):
-			delim = arg
-		elif opt in ('-l','--label'):
-			label = arg
-		elif opt in ('-u','--column'):
-			column = int(arg)
-		elif opt in ('--vline'):
-			line = float(arg)
-
+	parser.add_argument('--color', '-c', metavar='COLOR', required=False, dest='colour', type=str, default='#7D7D7D', help='Color of output histogram')
+	parser.add_argument('--title', '-t', metavar='TITLE', required=False, dest='title', type=str, default='Hisogram Of Data', help='Title of plot')
+	parser.add_argument('--xlab', '-x', metavar='X_LABEL', required=False, dest='x_lab', type=str, default='Value', help='Label for the x-axis')
 	
-	RawData = np.recfromcsv(InputFile,delimiter=delim)
-	x = [tuple[column] for tuple in RawData]
+	parser.add_argument('--ylab', '-y', metavar='Y_LABEL', required=False, dest='y_lab', default='Frequency', type=str, help='Label for the y-axis')
+	parser.add_argument('--delim', '-d', metavar='FILE_DELIMITER', required=False, dest='delim', type=str, help='Delimiter carachter in input file')
+	parser.add_argument('--label', '-l', metavar='DATA_LABEL', required=False, dest='label', type=str, default='data', help='Label data key')
+	
+	parser.add_argument('--column', '-u', metavar='DATA_COLUMN', required=False, dest='column', type=int, default=1, help='Column to use of data file')
+	parser.add_argument('--vline', metavar='VERTICAL_LINE', required=False, dest='vline', type=float, help='Adds a vertical line to plot at point on the x-axis')
+	parser.add_argument('--log', required=False, dest='log', action='store_true', help='Log (natural base) transform the input data')
 
-	pl.hist(x, bins=binsize,color=colour,label=label)
-	pl.xlabel(x_lab) ; pl.ylabel(y_lab) 
+	args = parser.parse_args()
+	
+	RawData = np.recfromcsv(args.InputFile,delimiter=args.delim)
+
+	if(args.log):
+		x = [math.log(tuple[args.column]) for tuple in RawData]
+	else:
+		x = [tuple[args.column] for tuple in RawData]
+		
+	pl.hist(x, bins=args.binsize,color=args.colour,label=args.label)
+	pl.xlabel(args.x_lab) ; pl.ylabel(args.y_lab) 
 	pl.legend(loc='upper left')
-	pl.title(title)
+	pl.title(args.title)
 	# make a histogram
 	
 	try:
-		line
-		pl.axvline(linewidth=2, color='r',x=line)
+		args.vline
+		pl.axvline(linewidth=2, color='r',x=args.vline)
 	except NameError:
-		line = 'none'
+		args.vline = 'none'
 		
 	# save the image to hardcopy
-	pl.savefig(Output)
+	pl.savefig(args.Output)
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+	main()
